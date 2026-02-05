cbe498478bc3d4d522dbd3efbfe50c8d57480eb9
MariusRask <mara1010@stud.ek.dk>
2026-02-05 11:01:39 +0100

    commit

--- src/TivoliRideTest.java
+++ src/TivoliRideTest.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class TivoliRideTest {
 
-    //hej
+    public static void main(String[] args) {
+
+
+        System.out.println("Find out if you're tall enough for the ride!");
+        System.out.print("Enter your height: ");
+
+        Scanner HeightChecker = new Scanner(System.in);
+        int height = HeightChecker.nextInt();
+System.out.println();
+        if (height < 170) {
+            System.out.println("You're not tall enough for the ride :(");
+            }
+        else {
+            System.out.println("You're tall enough for the ride ! :D");
+        }
+    }
 }
